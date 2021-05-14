4b924f350cc3151dc081f06062895d7d2f04176b
SoRePeSi <79937383+SoRePeSi@users.noreply.github.com>
2021-05-14 15:55:55 -0300

    Editar comentários

--- theProgram.js
+++ theProgram.js
@@ -1,9 +1,3 @@
-/* This was a school exercise
-	We were provided with the two accounts seen in clientes[]
-	We were asked to create a simple bank system in JavaScript that could execute the operations:
-	deposit, draft and transfer
-	This is my solution
-*/
 
 var clientes = [
     [nome = "Bruno",
